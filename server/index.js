0e27a8d88adaa53dcb7e540f5df344cda446003b
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-09-26 14:27:39 +0200

    Commit 7

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const authRouter = require("./routes/auth");
 const PORT = process.env.PORT || 3000;
 const app = express();
 const DB =
-  "mongodb+srv://jj:<password>@cluster0.8fy0ubk.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://jj:u7zJcX4LU4vFes0Y@cluster0.8fy0ubk.mongodb.net/?retryWrites=true&w=majority";
 
 // middleware
 app.use(express.json());
