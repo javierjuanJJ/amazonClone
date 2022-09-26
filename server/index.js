823bc6b602d2348625e75b9accc80e821d0e9ea2
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-09-26 14:27:20 +0200

    Commit 7

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 // IMPORTS FROM PACKAGES
 const express = require("express");
 const mongoose = require("mongoose");
-const adminRouter = require("./routes/admin");
+//const adminRouter = require("./routes/admin");
 // IMPORTS FROM OTHER FILES
 const authRouter = require("./routes/auth");
-const productRouter = require("./routes/product");
-const userRouter = require("./routes/user");
+//const productRouter = require("./routes/product");
+//const userRouter = require("./routes/user");
 
 // INIT
 const PORT = process.env.PORT || 3000;
