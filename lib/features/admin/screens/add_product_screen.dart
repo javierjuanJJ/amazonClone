f7ad88fabf5abe1b3ff575ad443206d8e73b53a1
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-09-28 14:11:09 +0200

    Commit 12

--- lib/features/admin/screens/add_product_screen.dart
+++ lib/features/admin/screens/add_product_screen.dart
@@ -1,13 +1,14 @@
 import 'dart:io';
 
 import 'package:amazon_clone/common/widgets/custom_button.dart';
-import 'package:amazon_clone/common/widgets/custom_textfield.dart';
 import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:amazon_clone/constants/utils.dart';
 import 'package:amazon_clone/features/admin/services/admin_services.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+
+import '../../../common/widgets/custom_text_field.dart';
 
 class AddProductScreen extends StatefulWidget {
   static const String routeName = '/add-product';
