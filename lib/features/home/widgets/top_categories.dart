65c69ff06718121575f093dbd4d79ca16ccd5a69
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-09-29 18:52:07 +0200

    Commit 14

--- lib/features/home/widgets/top_categories.dart
+++ lib/features/home/widgets/top_categories.dart
@@ -33,8 +33,8 @@ class TopCategories extends StatelessWidget {
                     child: Image.asset(
                       GlobalVariables.categoryImages[index]['image']!,
                       fit: BoxFit.cover,
-                      height: 40,
-                      width: 40,
+                      height: 30,
+                      width: 30,
                     ),
                   ),
                 ),
